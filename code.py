526f8235d01f3968d9ddf991134eb4436041c004
desmondrasie <desmond.rasie@live.ca>
2021-01-20 23:09:02 -0500

    Edited code to satisfy the conditions outlined

--- code.py
+++ code.py
@@ -1,18 +1,25 @@
 def are_valid_groups(students, groups):
     studentsFromGroups = []
+    temp = []
     #extract all group members in a single list
     for i in range(len(groups)):
-        for j in range(len(groups[i])):
-            studentsFromGroups.append(groups[i][j])
+        if len(groups[i]) == 2 or len(groups[i]) == 3:
+            for j in range(len(groups[i])):
+                studentsFromGroups.append(groups[i][j])
+        else:
+            return False
+    studentsFromGroups.sort()
+
+    for i in range(len(studentsFromGroups)-1):
+        if studentsFromGroups[i+1] == studentsFromGroups[i]:
+            return False
 
     return all(student in studentsFromGroups for student in students)
 
 def test():
-    students1 = [1, 2, 3, 4, 999]
-    students2 = [1, 2]
-
-    groups = [[1, 2], [3], [4, 5, 6], [7, 9], [99, 12]]
+    students1 = ["1", "2", "3", "4", "999"]
+    students2 = ["1", "2"]
+    groups = [["1", "2"], ["4", "5", "6"], ["8", "9"], ["99", "12"]]
     print(are_valid_groups(students1, groups))
     print(are_valid_groups(students2, groups))
-
 test()
